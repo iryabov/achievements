3de1ae716bc87deaa34a387c14751ecea039d08a
iryabov <iryabov@i-novus.ru>
2021-02-25 20:52:48 +0300

    Fixes

--- src/main/kotlin/com/github/iryabov/achievements/OrderChangeEvent.kt
+++ src/main/kotlin/com/github/iryabov/achievements/OrderChangeEvent.kt
@@ -1,24 +1,13 @@
 package com.github.iryabov.achievements
 
-import net.n2oapp.criteria.api.CollectionPage
 import net.n2oapp.criteria.dataset.DataSet
-import net.n2oapp.framework.api.exception.N2oException
-import net.n2oapp.framework.api.processing.DataProcessing
+import net.n2oapp.framework.api.processing.N2oModule
 import net.n2oapp.framework.api.ui.ActionRequestInfo
 import net.n2oapp.framework.api.ui.ActionResponseInfo
-import net.n2oapp.framework.api.ui.QueryRequestInfo
-import net.n2oapp.framework.api.ui.QueryResponseInfo
 import org.springframework.stereotype.Component
 
 @Component
-class OrderChangeEvent(val userService: UserService): DataProcessing {
-    override fun processAction(requestInfo: ActionRequestInfo<DataSet>?,
-                               responseInfo: ActionResponseInfo?,
-                               dataSet: DataSet?) {}
-
-    override fun processActionError(requestInfo: ActionRequestInfo<DataSet>?,
-                                    responseInfo: ActionResponseInfo?,
-                                    dataSet: DataSet?) {}
+class OrderChangeEvent(val userService: UserService): N2oModule() {
 
     override fun processActionResult(requestInfo: ActionRequestInfo<DataSet>?,
                                      responseInfo: ActionResponseInfo?,
@@ -27,16 +16,4 @@ class OrderChangeEvent(val userService: UserService): DataProcessing {
             requestInfo.user.set(userService.loadUserData(dataSet!!["email"] as String))
         }
     }
-
-    override fun processQuery(requestInfo: QueryRequestInfo?,
-                              responseInfo: QueryResponseInfo?) {}
-
-    override fun processQueryError(requestInfo: QueryRequestInfo?,
-                                   responseInfo: QueryResponseInfo?,
-                                   exception: N2oException?) {}
-
-    override fun processQueryResult(requestInfo: QueryRequestInfo?,
-                                    responseInfo: QueryResponseInfo?,
-                                    page: CollectionPage<DataSet>?) {}
-
 }
