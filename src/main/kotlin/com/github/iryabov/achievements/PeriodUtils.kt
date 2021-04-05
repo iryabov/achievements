5d8aed70b64f6ae152e332623ed63929e0edeafe
iryabov <iryabov@i-novus.ru>
2021-04-05 07:00:39 +0300

    Fixes and improvements

--- src/main/kotlin/com/github/iryabov/achievements/PeriodUtils.kt
+++ src/main/kotlin/com/github/iryabov/achievements/PeriodUtils.kt
@@ -7,12 +7,14 @@ import java.time.format.TextStyle
 
 
 data class Refbook(val id: Int, val name: String)
+
 const val START_YEAR = 2018
+const val START_MONTH = 5
 
 fun getMonths(): List<Refbook> {
     val result = ArrayList<Refbook>()
     for (i in 1..12) {
-        result.add(Refbook(i, getMonthName(i)))
+        result.add(getMonth(i))
     }
     return result
 }
@@ -22,21 +24,39 @@ fun getYears(): List<Refbook> {
     val current = LocalDate.now().year
     val result = ArrayList<Refbook>()
     for (i in start..current) {
-        result.add(Refbook(i, i.toString()))
+        result.add(getYear(i))
     }
     return result.sortedByDescending { it.id }
 }
 
-fun getMonthName(month: Int) = Month.of(month).getDisplayName(TextStyle.FULL_STANDALONE, LocaleContextHolder.getLocale()).capitalize()
+fun getPeriods(): List<Refbook> {
+    val startYear = START_YEAR
+    val startMonth = START_MONTH
+    val current = LocalDate.now().minusMonths(1)
+    val result = ArrayList<Refbook>()
+    for (y in startYear..current.year) {
+        val startOfMonth = if (y == START_YEAR) startMonth else 1
+        val endOfMonth = if (y == current.year) current.monthValue else 12
+        for (m in startOfMonth..endOfMonth) {
+            result.add(getPeriod(y * 100 + m))
+        }
+    }
+    return result.sortedByDescending { it.id }
+}
 
-fun getPeriod(year: Int, month: Int) = "${getMonthName(month)} $year"
+fun getMonthName(month: Int) =
+    Month.of(month).getDisplayName(TextStyle.FULL_STANDALONE, LocaleContextHolder.getLocale()).capitalize()
+
+fun getPeriodName(year: Int, month: Int) = "${getMonthName(month)} $year"
 
 fun getMonth(month: Int) = Refbook(month, getMonthName(month))
 
 fun getYear(year: Int) = Refbook(year, year.toString())
 
+fun getPeriod(yearmonth: Int) = Refbook(yearmonth, getPeriodName(yearmonth / 100, yearmonth % 100))
+
 fun getCurrentMonth() = getMonth(LocalDate.now().monthValue)
 
 fun getCurrentYear() = getYear(LocalDate.now().year)
 
-fun getCurrentPeriod() = getPeriod(LocalDate.now().year, LocalDate.now().monthValue)
+fun getCurrentPeriod() = getPeriodName(LocalDate.now().year, LocalDate.now().monthValue)
